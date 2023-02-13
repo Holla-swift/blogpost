16b27e87e124478a39049969aeef5d6d346801cd
Adeoti Sheriff <100189411+Holla-swift@users.noreply.github.com>
2023-02-13 23:32:30 +0100

    writing the view/html template

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import (
+    Author,
+    Category,
+    Post
+)
 
 # Create your views here.
+def homepage(request):
+    categories = Category.objects.all()[0:3]
+    featured = Post.objects.filter(featured=True)
+    latest = Post.objects.order_by('-timestamp')[0:3]
+        
+    context = {
+            'object_list': featured,
+            'latest': latest,
+            'categories': categories
+    }
+    return render (request, 'homepage.html', context)
+
+def post(request):
+    context = {
+        
+    }
+    return render (request, 'homepage.html', context)
+
+def about(request):
+    context = {
+        
+    }
+    return render (request, 'homepage.html', context)
+
+def category_post_list(request):
+    context = {
+        
+    }
+    return render (request, 'homepage.html', context)
+
+def all_posts(request):
+    context = {
+        
+    }
+    return render (request, 'homepage.html', context)
