2f7e40353e5b9074475bd9535def2de736b8a310
Adeoti Sheriff <100189411+Holla-swift@users.noreply.github.com>
2023-02-13 23:09:52 +0100

    made new migrations

--- blogpost/models.py
+++ blogpost/models.py
@@ -13,6 +13,19 @@ class Author(models.Model):
     def __str__(self):
         return self.user.username
 # Post
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    overview = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    thumbnail = models.ImageField()
+    categories = models.ManyToManyField(Category)
+    featured = models.BooleanField()
+
+    def __str__(self):
+        return self.title
 
 # Category
 
