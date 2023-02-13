03013a2755043359afed0c1df6325925b111e720
Adeoti Sheriff <100189411+Holla-swift@users.noreply.github.com>
2023-02-13 23:05:32 +0100

    first commit

---
+++ blogpost/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+# Author
+User = get_user_model()
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField()
+
+
+
+    def __str__(self):
+        return self.user.username
+# Post
+
+# Category
+
+
