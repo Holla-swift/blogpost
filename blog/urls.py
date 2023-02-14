cbe34780c596b909cd23e54e695f369c874c4ad6
Adeoti Sheriff <100189411+Holla-swift@users.noreply.github.com>
2023-02-14 11:37:25 +0100

    added static files

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from blogpost.views import (
     homepage, 
     post, 
@@ -19,3 +21,8 @@ urlpatterns = [
     path('post/', all_posts, name = 'all_posts'),
     path('search/', search, name = 'search'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
